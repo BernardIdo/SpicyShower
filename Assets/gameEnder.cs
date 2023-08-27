f7aec94b3278dfa8bcf8dc462c2bd1570e1ab956
ido bernard <ido.bernard@Gmail.com>
2023-08-28 01:09:04 +0300

    added gameEnder

--- Assets/gameEnder.cs
+++ Assets/gameEnder.cs
@@ -1,23 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class gameEnder : MonoBehaviour
 {
 
-    private PlayerController player;
-    private CameraScroll camera;
-    private bool shouldEndGame;
+    [SerializeField] private PlayerController player;
+    [SerializeField] private CameraScroll camera;
+    private bool _shouldEndGame;
+    private float _isTooFarEndGameDistance = -12f;
     
     // Start is called before the first frame update
     void Start()
     {
-        shouldEndGame = false;
+        _shouldEndGame = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var playerDistanceFromCamera = player.transform.position.y - camera.transform.position.y;
+        _shouldEndGame = playerDistanceFromCamera < _isTooFarEndGameDistance;
+        if (_shouldEndGame)
+        {
+            GameManager.instance.EndGame();
+        }
     }
 }
