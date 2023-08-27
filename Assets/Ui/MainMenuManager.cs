dcdb3576ab2201273fdd3a133915a6e5bd29c0a3
Nir feldman <nir1feld@gmail.com>
2023-08-28 01:09:50 +0300

    adds dg tween, tmpro

---
+++ Assets/Ui/MainMenuManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public CanvasGroup CanvasGroup;
+    public float fadeDuration = 0.5f;
+    
+    [ContextMenu("Fade")]
+    public void StartGame()
+    {
+        CanvasGroup.DOFade(0, fadeDuration).onComplete = () => ToggleClick(false);
+    }
+
+    private void ToggleClick(bool canClick)
+    {
+        CanvasGroup.interactable = canClick;
+    }
+
+    [ContextMenu("Fade in")]
+    public void Open()
+    {
+        ToggleClick(true);
+        CanvasGroup.DOFade(1, fadeDuration);
+    }
+}
