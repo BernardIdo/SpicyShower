4daab5be4972ecb19657bf7b78f049a1ad2c9e34
Nir feldman <nir1feld@gmail.com>
2023-08-28 15:29:02 +0300

    adds pause menu

--- Assets/Ui/MainMenuManager.cs
+++ Assets/Ui/MainMenuManager.cs
@@ -9,18 +9,22 @@ public class MainMenuManager : MonoBehaviour
 {
     public CanvasGroup CanvasGroup;
     public CanvasGroup scoreCanvasGroup;
+    public CanvasGroup pauseMenuCanvasGroup;
     public float fadeDuration = 0.5f;
+    private bool _paused;
 
     private void Start()
     {
         scoreCanvasGroup.alpha = 0;
+        pauseMenuCanvasGroup.interactable = false;
+        pauseMenuCanvasGroup.blocksRaycasts = false;
+        pauseMenuCanvasGroup.alpha = 0;
     }
 
     [ContextMenu("Fade")]
     public void StartGame()
     {
-        CanvasGroup.DOFade(0, fadeDuration).onComplete = () => ToggleClick(false);
-        scoreCanvasGroup.DOFade(1, fadeDuration);
+        ToggleCanvasGroup(CanvasGroup, false);
         GameManager.instance.StartPlayer();
     }
 
@@ -33,7 +37,64 @@ public class MainMenuManager : MonoBehaviour
     public void Open()
     {
         ToggleClick(true);
-        CanvasGroup.DOFade(1, fadeDuration);
-        scoreCanvasGroup.DOFade(0, fadeDuration);
+        ToggleCanvasGroup(CanvasGroup, true);
+    }
+
+    public void TogglePause()
+    {
+        if (_paused)
+        {
+            UnPause();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        _paused = true;
+        ToggleCanvasGroup(pauseMenuCanvasGroup, true);
+    }
+
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+        _paused = false;
+        ToggleCanvasGroup(pauseMenuCanvasGroup, false);
+    }
+
+    private void ToggleCanvasGroup(CanvasGroup canvasGroup, bool isOn)
+    {
+        if (isOn)
+        {
+            var tween =
+                canvasGroup.DOFade(1, fadeDuration);
+
+            tween.SetUpdate(true);
+            tween.onComplete = () =>
+            {
+                canvasGroup.blocksRaycasts = true;
+                canvasGroup.interactable = true;
+            };
+            
+            scoreCanvasGroup.DOFade(0, fadeDuration);
+
+        }
+        else
+        {
+            canvasGroup.blocksRaycasts = false;
+            canvasGroup.interactable = false;
+            scoreCanvasGroup.DOFade(1, fadeDuration).SetUpdate(true);
+            canvasGroup.DOFade(0, fadeDuration);
+        }
+    }
+
+    public void RestartGame()
+    {
+        UnPause();
+        GameManager.instance.StartPlayer();
     }
 }
